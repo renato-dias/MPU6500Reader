87eaac0cefddfda0323ef7bf27d3375f0bc3244a
Renato Dias <renato.lourenco.dias@tecnico.ulisboa.pt>
2022-12-22 17:51:26 +0000

    Getting CSVs from serial and plot the time/frequency response

--- simple_imu_code/simple_imu_code.ino
+++ simple_imu_code/simple_imu_code.ino
@@ -1,72 +1,65 @@
-/*
-* Brian R Taylor
-* brian.taylor@bolderflight.com
-* 
-* Copyright (c) 2021 Bolder Flight Systems Inc
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the “Software”), to
-* deal in the Software without restriction, including without limitation the
-* rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-* sell copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-* IN THE SOFTWARE.
-*/
+#include <SPI.h>
 
-#include "mpu6500.h"
+#define MPU6500_I2C_ADDRESS 0x68
+#define MPU6500_REG_ACCEL_CONFIG 0x1C
+#define MPU6500_ACCEL_FS_2G 0x00
+#define MPU6500_ACCEL_FS_4G 0x08
+#define MPU6500_ACCEL_FS_8G 0x10
+#define MPU6500_ACCEL_FS_16G 0x18
+    // ±2 g: 0x00
+    // ±4 g: 0x08
+    // ±8 g: 0x10
+    // ±16 g: 0x18
 
-/* Mpu6500 object */
-bfs::Mpu6500 imu;
+const int MPU_CS = 10;  // Chip select pin for the MPU
 
 void setup() {
-  /* Serial to display data */
+  // Initialize the SPI interface and the MPU
   Serial.begin(2000000);
-  while(!Serial) {}
-  /* Start the I2C bus */
-  Wire.setClock(400000);
-  Wire.begin();
-  /* I2C bus,  0x68 address */
-  imu.Config(&Wire, bfs::Mpu6500::I2C_ADDR_PRIM);
-  /* Initialize and configure IMU */
-  if (!imu.Begin()) {
-    Serial.println("Error initializing communication with IMU");
-    while(1) {}
-  }
-  /* Set the sample rate divider */
-  if (!imu.ConfigSrd(19)) {
-    Serial.println("Error configured SRD");
-    while(1) {}
-  }
+  SPI.begin();
+  pinMode(MPU_CS, OUTPUT);
+  digitalWrite(MPU_CS, HIGH);
+  initMPU();
 }
 
 void loop() {
-  /* Check if data read */
-  if (imu.Read()) {
-    Serial.print(millis());
-    Serial.print(",");
-    Serial.print(imu.accel_x_mps2());
-    Serial.print(",");
-    Serial.print(imu.accel_y_mps2());
-    Serial.print(",");
-    Serial.print(imu.accel_z_mps2());
-    Serial.println();
-    // Serial.print(imu.gyro_x_radps());
-    // Serial.print("\t");
-    // Serial.print(imu.gyro_y_radps());
-    // Serial.print("\t");
-    // Serial.print(imu.gyro_z_radps());
-    // Serial.print("\t");
-    // Serial.print(imu.die_temp_c());
-    // Serial.print("\n");
-  }
+  // Read the raw acceleration data from the MPU
+  int16_t ax, ay, az;
+  readAccelerationData(ax, ay, az);
+
+  // Print the acceleration data to the serial console
+  Serial.print(micros());
+  Serial.print(",");
+  Serial.print(ax);
+  Serial.print(",");
+  Serial.print(ay);
+  Serial.print(",");
+  Serial.println(az);
+}
+
+void initMPU() {
+  // Put the MPU into sleep mode
+  writeMPU(0x6B, 0x00);
+  // Set the accelerometer full scale range to +/- 2g
+  writeMPU(MPU6500_REG_ACCEL_CONFIG, MPU6500_ACCEL_FS_16G);
+  // Set the gyro full scale range to +/- 250 degrees/sec
+  writeMPU(0x1B, 0x00);
+}
+
+void readAccelerationData(int16_t& ax, int16_t& ay, int16_t& az) {
+  // Read the raw acceleration data from the MPU
+  digitalWrite(MPU_CS, LOW);
+  SPI.transfer(0x3B | 0x80);  // Read from the ACCEL_XOUT_H register
+  ax = ((int16_t)SPI.transfer(0x00) << 8) | SPI.transfer(0x00);
+  ay = ((int16_t)SPI.transfer(0x00) << 8) | SPI.transfer(0x00);
+  az = ((int16_t)SPI.transfer(0x00) << 8) | SPI.transfer(0x00);
+  digitalWrite(MPU_CS, HIGH);
+}
+
+void writeMPU(uint8_t reg, uint8_t data) {
+  // Write a value to a register on the MPU
+  digitalWrite(MPU_CS, LOW);
+  SPI.transfer(reg);
+  SPI.transfer(data);
+  digitalWrite(MPU_CS, HIGH);
 }
